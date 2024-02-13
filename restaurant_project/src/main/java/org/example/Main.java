249ec725ada21a603e4ce956e909635271d3bc4f
arkanlems <estebanlemuspena@gmail.com>
2024-02-13 15:29:32 -0500

    example reverse first commit

--- restaurant_project/src/main/java/org/example/Main.java
+++ restaurant_project/src/main/java/org/example/Main.java
@@ -6,6 +6,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        List<Integer> lista = new ArrayList<>();
     }
 }
